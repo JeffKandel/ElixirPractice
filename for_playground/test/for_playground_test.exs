25cf692477393187baf6118e8f328e1a59862410
Jeff Kandel <jeff.kandel@gmail.com>
2018-04-21 09:51:38 -0400

    for comprehensions

--- for_playground/test/for_playground_test.exs
+++ for_playground/test/for_playground_test.exs
@@ -1,8 +1,24 @@
 defmodule ForPlaygroundTest do
   use ExUnit.Case
-  doctest ForPlayground
 
-  test "greets the world" do
-    assert ForPlayground.hello() == :world
+  test "basic for comprehensions" do
+    result = for i <- [1,2,3], do: i + 1
+    assert [2,3,4] == result
+  end
+
+  test "filters in for comprehensions" do
+    result =
+      for i <- 0..10, rem(i,2) == 1 do
+        i
+      end
+    assert result = [1,3,5,7,9]
+  end
+
+  test "collecting results into a map" do
+    result =
+      for i <- 0..2, into: %{} do
+        {"#{i}", i}
+      end
+    assert result == %{"0" => 0, "1" => 1, "2" => 2}
   end
 end
