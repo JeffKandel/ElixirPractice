946725f6673c514c78dd0405d7dc28a2b5201d67
Jeff Kandel <jeff.kandel@gmail.com>
2018-04-22 08:39:14 -0400

    not found test

--- otp_playground/test/otp_playground_test.exs
+++ otp_playground/test/otp_playground_test.exs
@@ -12,4 +12,9 @@ defmodule OtpPlaygroundTest do
     GenServer.call(fridge, {:store,:bacon})
     assert {:ok, :bacon} == GenServer.call(fridge, {:take, :bacon})
   end
+
+  test "doesn't exist" do
+    {:ok, fridge} = GenServer.start_link FridgeServer, [], []
+    assert :not_found == GenServer.call(fridge, {:store,:bacon})
+  end
 end
